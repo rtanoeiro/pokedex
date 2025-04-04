d6013bc79413910b9338ca1c6c6bb05a9adb82b8
rtanoeiro <ramontanoeiro@gmail.com>
2025-04-04 23:15:39 +1100

    updated catch command so it gets all data for the pokemon when adding to pokedex

--- commands_catch.go
+++ commands_catch.go
@@ -6,55 +6,98 @@ import (
 	"math/rand"
 	"pokedexcli/internal/pokecache"
 )
+
 func commandCatch(config *Config, cache *pokecache.Cache, myPokedex *Pokedex) {
 
 	pokemonName := config.params
-	url:=PokemonURL+pokemonName
+	url := PokemonURL + pokemonName
 	resData, err := getRequest(url)
 
 	if err != nil {
 		fmt.Println("Unable to find Pokemon", pokemonName, ". Error:", err)
 	}
-	
-	pokemons := Pokemon{}
-	errUM := json.Unmarshal(resData, &pokemons)
-	
-	if errUM!= nil {
+
+	thisPokemon := Pokemon{}
+	errUM := json.Unmarshal(resData, &thisPokemon)
+
+	if errUM != nil {
 		fmt.Println("Error unmarshaling data: ", errUM)
 	}
 
 	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
-	baseDifficulty := pokemons.BaseExperience
-	catchRate := float64(baseDifficulty)/256.0
+	baseDifficulty := thisPokemon.BaseExperience
+	catchRate := float64(baseDifficulty) / 256.0
 	randomNumber := rand.Float64()
-
 	var exists bool
 	if randomNumber >= catchRate {
 		exists = checkPokedex(pokemonName, myPokedex)
 		fmt.Printf("%s was caught!\n", pokemonName)
 		if !exists {
-			addToPokedex(pokemonName, myPokedex)
-		} else {
-			updatedPokedex(pokemonName, myPokedex)
+			addToPokedex(thisPokemon, pokemonName, myPokedex)
 		}
 	} else {
 		fmt.Printf("%s escaped!\n", pokemonName)
 	}
-
-	
+	return
 }
 
-func checkPokedex(pokemon string, pokedex *Pokedex) bool {
+func checkPokedex(pokemonName string, pokedex *Pokedex) bool {
 
-	_, ok := (*pokedex)[pokemon]
+	_, ok := (*pokedex)[pokemonName]
 
 	return ok
 }
 
-func addToPokedex(pokemon string, pokedex *Pokedex) {
-	(*pokedex)[pokemon] = 1
+func addToPokedex(pokemonDetails Pokemon, pokemonName string, pokedex *Pokedex) {
+
+	hp, atk, def, spatk, spdef, spd := getPokemonStats(pokemonDetails, pokemonName)
+	types := getPokemonTypes(pokemonDetails, pokemonName)
+
+	pokemon := (*pokedex)[pokemonName]
+	pokemon.Height = pokemonDetails.Height
+	pokemon.Weight = pokemonDetails.Weight
+	pokemon.Stats.hp = hp
+	pokemon.Stats.attack = atk
+	pokemon.Stats.defense = def
+	pokemon.Stats.specialAttack = spatk
+	pokemon.Stats.specialDefense = spdef
+	pokemon.Stats.speed = spd
+
+	if len(types) == 2 {
+		pokemon.Types.type1 = types[0]
+		pokemon.Types.type2 = types[1]
+	} else {
+		pokemon.Types.type1 = types[0]
+	}
+
+	(*pokedex)[pokemonName] = pokemon
+
+	return
 }
 
-func updatedPokedex(pokemon string, pokedex *Pokedex) {
-	(*pokedex)[pokemon]++
+func getPokemonStats(pokemonDetails Pokemon, pokemonName string) (int, int, int, int, int, int) {
+
+	pokemonStats := pokemonDetails.Stats
+
+	hp := pokemonStats[0].BaseStat
+	atk := pokemonStats[1].BaseStat
+	defense := pokemonStats[2].BaseStat
+	spatk := pokemonStats[3].BaseStat
+	spdef := pokemonStats[4].BaseStat
+	spd := pokemonStats[5].BaseStat
+
+	return hp, atk, defense, spatk, spdef, spd
+}
+
+func getPokemonTypes(pokemonDetails Pokemon, pokemonName string) []string {
+
+	pokemonStats := pokemonDetails.Types
+
+	var pokemonTypes []string
+
+	for _, pokemonType := range pokemonStats {
+		pokemonTypes = append(pokemonTypes, pokemonType.Type.Name)
+	}
+
+	return pokemonTypes
 }
